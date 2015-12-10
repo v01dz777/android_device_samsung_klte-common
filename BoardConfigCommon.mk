eb7725e121fe35a2d6b734143406f5f511517c41
sakuramilk <c.sakuramilk@gmail.com>
2015-12-10 16:58:06 +0900

    add BOARD_HAVE_LEGACY_BODY_SENSOR flag
    Change-Id: Id17668149d1fdfb1ba129570b6b4cbd909a09494

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,5 +95,8 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
 
+# Sensor
+BOARD_HAVE_LEGACY_BODY_SENSOR := true
+
 # inherit from the proprietary version
 -include vendor/samsung/klte-common/BoardConfigVendor.mk
