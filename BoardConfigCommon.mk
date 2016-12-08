5489f67eda84f0575efea38bcc2d761d55f4b826
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-12-07 19:56:39 -0600

    klte-common: compress recovery ramdisk with lzma
    Change-Id: I1e590c22f0a68ecdbbb1c8057d6e55d9ec43b79f
    
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,11 +34,9 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-# TARGET_KERNEL_CONFIG := msm8974_sec_defconfig
-# TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+LZMA_RAMDISK_TARGETS := recovery
+#TARGET_KERNEL_SOURCE := kernel/samsung/msm8974
 TARGET_KERNEL_SOURCE := kernel/samsung/klte
-TARGET_KERNEL_HAVE_EXFAT := true
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Audio
 BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
