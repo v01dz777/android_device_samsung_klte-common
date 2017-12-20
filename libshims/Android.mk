1f980114932957be0c2457ec776582cf5a988c13
ma34s <ma34s.sss@gmail.com>
2017-12-21 00:05:04 +0900

    [camera_shim]change c to cpp
    Change-Id: Id3a8c3da7c12b8a4285c8af6f8f74bd2f8dee3e9

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := camera_shim.c
+LOCAL_SRC_FILES := camera_shim.cpp
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
